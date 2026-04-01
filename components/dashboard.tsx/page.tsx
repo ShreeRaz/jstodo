2d7f4e2f7830a71dbfb15e8042a6a8f472e4823c
ShreeRaz <ankitt60raz@gmail.com>
2026-04-01 23:03:58 +0545

    Add and fetch data.json

--- components/dashboard.tsx/page.tsx
+++ components/dashboard.tsx/page.tsx
@@ -13,10 +13,27 @@ const posts = postsData as Post[];
 
 export default function Dashboard() {
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.text}</li>
-      ))}
-    </ul>
+    <div>
+      <div className="p-4 ">
+        <h1 className="text-3xl font-bold">JS Problem Tracker</h1>
+        <p>
+          60 problems to take you from beginner to proficient. Check off as you
+          go.
+        </p>
+      </div>
+
+      {/* filter */}
+      <div></div>
+
+      {/* progressbar */}
+
+      <div></div>
+
+      <ul className="flex flex-col gap-4 justify-center items-center">
+        {posts.map((post) => (
+          <li key={post.id}>{post.text}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
